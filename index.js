041ec19763736d6120e8d7cbf9be9ba9771ec20b
sozi <sozyarnadir@gmail.com>
2022-08-24 20:01:32 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // To pass the tests, don't forget to return your fetch!
+
+    return fetch("https://anapioficeandfire.com/api/books")
+        .then((resp) => resp.json())
+        .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    function renderBooks(books) {
+        document.addEventListener("DOMContentLoaded", function() {
+            fetchBooks();
+        });
+    }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
